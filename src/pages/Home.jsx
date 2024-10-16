0fafed42ca4b4a92af22c1e47e7196b5ad813fd0
loficrypto <lofiking810@gmail.com>
2024-10-16 15:58:09 +0600

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold">Welcome to My Shop</h1>
+        <p className="text-lg mt-4">Buy the best digital products with cryptocurrency.</p>
+    </div>
+);
+
+export default Home;
