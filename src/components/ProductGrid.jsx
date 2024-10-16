c400ac8388be377e9de67192d232d3e10ee0d43d
loficrypto <lofiking810@gmail.com>
2024-10-16 16:56:13 +0600

    Update ProductGrid.jsx

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,9 +1,9 @@
 import ProductCard from './ProductCard';
 
-const ProductGrid = ({ products, handlePurchase }) => (
+const ProductGrid = ({ products, handleAddToCart }) => (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map(product => (
-            <ProductCard key={product.id} product={product} handlePurchase={handlePurchase} />
+            <ProductCard key={product.id} product={product} handleAddToCart={handleAddToCart} />
         ))}
     </div>
 );
